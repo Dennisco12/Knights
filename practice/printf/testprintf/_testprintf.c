a64e141572679f6389bf1b62637f674e7222cf52
Dennisco12 <akinwonjowodennisco@gmail.com>
2022-05-23 13:41:07 +0100

    updated

--- practice/printf/testprintf/_testprintf.c
+++ practice/printf/testprintf/_testprintf.c
@@ -5,9 +5,46 @@
  * return: number of char printed
  */
 
-int _printf(const char format, ...)
+int _printf(const char *format, ...)
 {
 	int count;
 	va_list elem;
 
-	va_start
+	va_start(elem, format);
+
+	count = 0;
+	while (format[count])
+	{
+		if (format[count] == '%')
+		{
+			count++;
+
+			if (format[count] == '%')
+			{
+				oogwe ('%');
+				break;
+			}
+			else
+			{
+				switch (format[count])
+				case 'd':
+				{
+					print_int(va_arg(elem, int));
+					break;
+				}
+				case 's':
+				{
+					print_string(va_arg(elem, char *));
+					break;
+				}
+				case 'i':
+				{
+					print_num(va_arg(elem, int));
+					break;
+				}
+				case 'c':
+				{
+					print_char(va_arg(elem, int));
+					break;
+				}
+			}
